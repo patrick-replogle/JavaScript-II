14ddc3a3e4f2fe7d2b3ca1293749123d8b0e3eeb
patrick-replogle <patrickr1138@gmail.com>
2019-08-08 20:16:05 -0700

    mvp and most of stretch done.  Might try for last stretch questions.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,17 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+
+//example 1: 
+const name1 = "Dave";
+
+function reference(){
+  return `I am ${name1}`;
+}
+
+console.log(reference());
+
+//example 2:
+
 function first(){
   const name = "Patrick";
   console.log(`I am ${name}`);
@@ -7,8 +19,7 @@ function first(){
 
   function second(){
     console.log(`I am also ${name}`);
-    debugger;
-    
+    debugger; 
   }
   second();
 }
@@ -19,15 +30,27 @@ console.log(first());
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+
+var counter = (function () {
+  var newCounter = 0;
+  return function () {
+    newCounter += 1; 
+    return newCounter;
+  }
+})();
+
+counter();
+counter();
+counter();
+// Return a function that when invoked increments and returns a counter variable.
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+
+
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
